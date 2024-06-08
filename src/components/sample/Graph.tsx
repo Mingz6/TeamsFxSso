57b7355fc2c1c4a9fb4a6bf9c0f0f71a73fa8654
Ming <Mingz6@outlook.com>
2024-06-07 19:22:54 -0600

    Sample

--- src/components/sample/Graph.tsx
+++ src/components/sample/Graph.tsx
@@ -39,25 +39,78 @@ export function Graph() {
       return { profile, photoUrl };
     },
     {
-      scope: ["User.Read","profile"],
+      scope: ["User.Read", "profile"],
       credential: teamsUserCredential,
     }
   );
+
+  // Method 2: get Teams token silently
+  // teamsUserCredential!
+  //   .getToken([])
+  //   .then(async (tokenResponse) => {
+  //     if (!tokenResponse) {
+  //       // Handle the case where silent authentication fails
+  //       console.warn("Silent authentication failed. No token received.");
+  //     } else {
+  //       // console.log('teams token: ', tokenResponse.token);
+  //       const response = await fetch(
+  //         "https://<MyDomain>.ngrok.io/api/auth/SingleSignOn",
+  //         {
+  //           method: "POST",
+  //           headers: {
+  //             "Content-Type": "application/json",
+  //             Authorization: `Bearer ${tokenResponse.token}`,
+  //           },
+  //           body: JSON.stringify({
+  //             clientId: reactAppClientId,
+  //             scopes: ["openid", "email", "User.Read", "offline_access"],
+  //           }),
+  //         }
+  //       ).then((res) => res.json());
+
+  //       // console.log('fetch response: ', response.access_token);
+  //       localStorage.setItem(AccessTokenTypes.<TeamsAppType>, response.access_token);
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     console.error("Silent authentication failed", error);
+  //   });
+
+  // Method 3: get Teams token using authProvider
+  // provider.graph.client.api('/me').get().then((profile) => {
+  //   console.log('graph1 profile', profile);
+  // });
+
+  // const authProvider = new TokenCredentialAuthenticationProvider(credential, {
+  //   scopes: ['User.Read'],
+  // });
+  // const graph = Client.initWithMiddleware({
+  //   authProvider: authProvider,
+  // });
+
+  // const profile = await graph.api('/me').get();
+  // console.log('authProvider profile', profile);
+  // const graphToken = await authProvider.getAccessToken();
+  // console.log('authProvider token: ', graphToken);
 
   return (
     <div>
       <Design />
       <h3>Example: Get the user's profile</h3>
       <div className="section-margin">
-        <p>Click below to authorize button to grant permission to using Microsoft Graph.</p>
+        <p>
+          Click below to authorize button to grant permission to using Microsoft
+          Graph.
+        </p>
         <pre>{`credential.login(scope);`}</pre>
         <Button appearance="primary" disabled={loading} onClick={reload}>
           Authorize
         </Button>
 
         <p>
-          Below are two different implementations of retrieving profile photo for currently
-          signed-in user using Fluent UI component and Graph Toolkit respectively.
+          Below are two different implementations of retrieving profile photo
+          for currently signed-in user using Fluent UI component and Graph
+          Toolkit respectively.
         </p>
         <h4>1. Display user profile using Fluent UI Component</h4>
         <PersonCardFluentUI loading={loading} data={data} error={error} />
